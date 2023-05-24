b9c35aa8eba4d84fbd29fbaa5a2afdcdee5b4d60
brodsa <brodinova@gmail.com>
2023-05-24 22:54:16 +0000

    feat: add a basic style and layout of the cards

---
+++ assets/js/game.js
@@ -0,0 +1,18 @@
+
+function generateBoard(){
+    const gameBoard = document.querySelector(".game__board");
+
+    for(let i=1; i <= 12; i++){
+        const card = document.createElement('div');
+        const cardFront = document.createElement('div');
+        const cardBack = document.createElement('div');
+    
+        card.classList.add('card');
+        cardFront.classList.add('card__front');
+        cardBack.classList.add('card__back');
+
+        gameBoard.append(card);
+    }
+}
+generateBoard()
+
