0b094f4e9f2c93b4487648eed652675d60fea777
brodsa <brodinova@gmail.com>
2023-05-25 22:17:49 +0000

    feat: write function to shuffle the cards data

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,7 @@
+// get the data using fetch - code from the slack CI community
+const data = await fetch("assets/data/data.json").then(res => res.json());
+console.log(data);
+
 
 function generateBoard(){
     const gameBoard = document.querySelector(".game__board");
@@ -18,5 +22,37 @@ function generateBoard(){
         
     }
 }
-generateBoard()
+generateBoard();
 
+const shuffleCards = () => {
+    // https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
+    const shuffledData = data.sort(() => 0.5 - Math.random());
+    const selectedData = shuffledData.slice(0,6);
+    return selectedData;
+}
+
+const cardsData = shuffleCards()
+
+
+
+
+
+
+
+// const shuffledCards = () => {
+//     // get the data using fetch - code from the slack CI community
+//     fetch("assets/data/data.json")
+//     .then(res => res.json())
+//     .then(data => {
+//         // https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
+//         const shuffledData = data.sort(() => 0.5 - Math.random());
+//         const selectedData = shuffledData.slice(0,6);
+//         console.log(selectedData);
+//         return selectedData;
+//     }
+//     )
+// }
+
+
+// import data from '../data/data.json' assert { type: 'json' };
+// console.log(data);
