c42e779c16cc63a618a958c1e84d0dbf0ffa1253
brodsa <brodinova@gmail.com>
2023-05-26 12:50:12 +0000

    feat: modify the logic to select/shuffle the data cards from json, see selectRandomCards()

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,50 @@
-// get the data using fetch - code from the slack CI community
+// get the data using fetch function from the slack CI community
 const data = await fetch("assets/data/data.json").then(res => res.json());
-console.log(data);
+
+/**
+ * Select random cards based on the given number of pairs (numOfCards). 
+ * For the game, the numOfCards stays fixed.
+ * Returns an object of 6 card pairs (6 images and 6 text).
+ * @param {*} numOfCards 
+ * @returns 
+ */
+
+const selectRandomCards = (numOfPairs=6) => {
+    // shuffle the array elements from https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
+    const shuffledCards = data.sort(() => 0.5 - Math.random());
+    const selectedBasis = shuffledCards.slice(0,numOfPairs);
+
+    const selectedImg = []
+    const selectedTxt = []
+    for(let item of selectedBasis){
+        item["type"] = "txt"
+        selectedTxt.push(item);
+        // how to clone the object from https://www.freecodecamp.org/news/clone-an-object-in-javascript/
+        let element = { ... item }
+        element["type"] = "img"
+        selectedImg.push(element);
+    }
+    
+    const shuffled = selectedTxt.concat(selectedImg)
+    const shuffledData = shuffled.sort(() => 0.5 - Math.random());
+    console.log(shuffledData);
+    return shuffledData;
+}
 
 
-function generateBoard(){
+function generateBoard(numOfCards=12){
     const gameBoard = document.querySelector(".game__board");
+    const cardsData = selectRandomCards();
 
-    for(let i=1; i <= 12; i++){
+    for(let i=0; i < numOfCards; i++){
         const card = document.createElement('div');
         const cardFront = document.createElement('div');
         const cardBack = document.createElement('div');
     
         card.classList.add('card');
         cardFront.classList.add('card__front');
-        cardFront.innerText = 'Test'
         cardBack.classList.add('card__back');
+
 
         gameBoard.append(card);
         card.append(cardFront);
@@ -24,14 +54,9 @@ function generateBoard(){
 }
 generateBoard();
 
-const shuffleCards = () => {
-    // https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
-    const shuffledData = data.sort(() => 0.5 - Math.random());
-    const selectedData = shuffledData.slice(0,6);
-    return selectedData;
-}
 
-const cardsData = shuffleCards()
+
+
 
 
 
