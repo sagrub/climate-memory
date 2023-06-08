9a92b45881b8623fb4c460cc4a181d467d4d2cab
brodsa <brodinova@gmail.com>
2023-06-08 18:07:16 +0000

    style: add additional comments to the addListenerOnButtons

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,7 +40,7 @@ const makeBackgroundNormal = () => {
 };
 
 /**
- * Add event listner on the buttons depending on the their 
+ * Add a click event listner on the buttons depending on the their 
  * data attributes.
  */
 const addListenerOnButtons = () => {
@@ -52,30 +52,38 @@ const addListenerOnButtons = () => {
             
             switch(buttonType){
                 case 'btn-open-how-to':
+                    // open the modal How to play and freeze the board
                     openModal('modal-how-to');
                     makeBackgroundDark();
                     break;
                 case 'btn-open-play':
+                    // direct to the game page
                     window.location.href = "game.html";
                     break;
                 case 'btn-open-story':
+                    // open the modal Story behind and freeze the board
                     openModal('modal-story');
                     makeBackgroundDark();
                     break;
                 case  'btn-close':
+                    // all close buttons close the modal and unfreeze the board
                     closeModal();
                     makeBackgroundNormal();
                     break;
                 case  'btn-home':
+                    // direct to the homepage
                     window.location.href = "index.html";
                     break;
                 case 'btn-homepage':
+                    // direct to the homepage from the hidden pages
                     window.location = "https://brodsa.github.io/climate-memory/";
                     break;
                 case  'btn-refresh':
+                    // reload the gameboard to refresh the game
                     window.location.href = "game.html";
                     break;
                 case'btn-contact':
+                    // open the modal Contact me and freeze the board
                     openModal('modal-contact');
                     makeBackgroundDark();
                     break;
